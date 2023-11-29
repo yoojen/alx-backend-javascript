c9d2b3c59735f41318d4722a6296acda2e12f8a7
yoojen <eugenemutuyimana15@gmail.com>
2023-11-29 12:30:07 +0000

    returned all promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => (
+      data.map((res) => 
+        [{status: res.status, value: res.value}] 
+      )
+    ));
+}

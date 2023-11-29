bbbb7c727bf5f3880ea0fa107753b79ab39433ec
Eugene <140322568+yoojen@users.noreply.github.com>
2023-11-29 13:13:13 +0200

    changed params names

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-   .all([uploadPhoto(), createUser()])
-   .then((data) => {
-     console.log(`${data[0].body} ${data[0].firstName} ${data[1].lastName}`);
-   })
-   .catch(() => console.log("Signup system offline"));
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }

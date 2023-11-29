1acc6b9020ee93c4ed76e1cda511d9253c160458
yoojen <eugenemutuyimana15@gmail.com>
2023-11-29 12:43:20 +0000

    iterated over more than one promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((data) => (
-      data.map((res) => 
-        [{status: res.status, value: res.value}] 
-      )
+      data.map((res) => ({
+	status: res.status,
+	value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }))
     ));
 }

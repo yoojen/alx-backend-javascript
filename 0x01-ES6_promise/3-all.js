1feab15e180933d8b46f50e2490ded6ca5a783d5
yoojen <eugenemutuyimana15@gmail.com>
2023-11-29 11:06:19 +0000

    removed repating then handlers

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils'
 export default function handleProfileSignup() {
-  uploadPhoto().then((data_1)=>{
-    createUser().then((data_2)=>{
-      console.log(data_1.body, data_2.firstName, data_2.lastName);
-    })
-  }).catch(() => console.log("Signup system offline")
+  return Promise.all([uploadPhoto(), createUser()])
+   .then((data) => console.log(`${data[0].body} ${data[0].firstName} ${data[1].lastName}`));
+   .catch(() => console.log("Signup system offline"));
 }
 

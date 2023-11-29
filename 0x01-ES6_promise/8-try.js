8ae82e61cf9d512aa8614d448c8d4bc6ef9c873c
Eugene <140322568+yoojen@users.noreply.github.com>
2023-11-29 15:17:13 +0200

    changed scripting style

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator !== 0) {
     return numerator / denominator;
-  }
-  else {
+  } else {
     throw Error("cannot divide by 0");
   }
 }

3e27524fae4caf55c49c73982b697d22edc10324
yoojen <eugenemutuyimana15@gmail.com>
2023-12-02 20:45:15 +0000

    building class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('Expecting a number');
+    }
+    this._sqft = value;
+  }
+
+  get sqft() {
+    return this.sqft;
+  }
+}

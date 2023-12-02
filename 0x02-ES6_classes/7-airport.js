4920bbdf5e1325c3ae683af469e2e5ce7a13d256
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-03 00:21:13 +0200

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,25 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
+    this.name = name;
+    this.code = code;
+  }
+  get name() {
+    return this._name;
   }
 
-  toString() {
-    return `${this._code}`;
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
   }
 }

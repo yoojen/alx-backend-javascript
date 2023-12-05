57361a8a037f8243c0a6054dfc35fd29003eb78f
yoojen <eugenemutuyimana15@gmail.com>
2023-12-05 22:27:25 +0000

    cleans et

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,11 @@ export default function cleanSet(set, startString) {
   const fullString = [];
 
   if (!set || startString === '' || typeof startString !== 'string' || !(set instanceof Set)) {
-    return ";
+    return '';
   }
   set.forEach((element) => {
     if (typeof element === 'string' && element.startsWith(startString)) {
-       if (element !== value) {
       fullString.push(element.substring(startString.length));
-      }
     }
   });
     return fullString.join('-');

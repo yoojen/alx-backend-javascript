b9f6334c57c350f2f20d740bc61fc9b50270ce53
Eugene <eugenemutuyimana15@gmail.com>
2024-01-31 22:01:29 +0200

    files passed eslint test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   file_data = [];
@@ -6,31 +6,31 @@ function countStudents(path) {
   let occurance = {};
 
   if (!fs.existsSync(path)) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
   if (!fs.statSync(path).isFile()) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
-  data = fs.readFileSync(path, "utf-8").split("\n");
+  data = fs.readFileSync(path, 'utf-8').split('\n');
   fields = data[0];
   for (let i = 0; i < data.length; i++) {
     if (i == 0) {
       continue;
     }
-    if (data[i] == "\r" || data[i].length <= 0) {
+    if (data[i] == '\r' || data[i].length <= 0) {
       break;
     } else {
-      first_name = data[i].split(",")[0];
-      last_name = data[i].split(",")[1];
-      age = data[i].split(",")[2];
-      field = data[i].split(",")[3].replace("\r", "");
+      first_name = data[i].split(',')[0];
+      last_name = data[i].split(',')[1];
+      age = data[i].split(',')[2];
+      field = data[i].split(',')[3].replace('\r', '');
       NUMBER_OF_STUDENTS += 1;
       file_data.push({ first_name, last_name, age, field });
     }
   }
 
   file_data.map((person) => {
-    field = person["field"];
+    field = person['field'];
     if (!occurance[field]) {
       occurance[field] = [person];
     } else {
@@ -42,7 +42,7 @@ function countStudents(path) {
     let count = 0;
     let names = [];
     for (let i = 0; i < occurance[key].length; i++) {
-      names.push(" " + occurance[key][i].first_name);
+      names.push(' ' + occurance[key][i].first_name);
       count++;
     }
     console.log(

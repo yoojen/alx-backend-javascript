ea780c7fbc87bb847e0381f1842198472fda5ade
Eugene <eugenemutuyimana15@gmail.com>
2024-01-31 22:59:18 +0200

    exported the app

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const PORT = 1245;
-const HOST_NAME = '127.0.0.1';
 app = express();
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
-app.listen(PORT, HOST_NAME, () => {
-  process.stdout.write(`Server running on: ${HOST_NAME}:${PORT}`);
+
+app.listen(PORT, () => {
+  process.stdout.write(`Server running on: ${PORT}`);
 });
+
+module.exports = app;

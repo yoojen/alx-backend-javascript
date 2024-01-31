1de958e79fdbd997453d653665c48e938bf35bc0
Eugene <eugenemutuyimana15@gmail.com>
2024-01-31 22:03:46 +0200

    changed to single qoute

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const PORT = 1245;
-const HOST_NAME = "127.0.0.1";
+const HOST_NAME = '127.0.0.1';
 app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 app.listen(PORT, HOST_NAME, () => {
   process.stdout.write(`Server running on: ${HOST_NAME}:${PORT}`);
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const PORT = 1245;
-const HOST_NAME = "127.0.0.1";
-const FILE_PATH = process.argv.length > 2 ? process.argv[2] : "";
+const HOST_NAME = '127.0.0.1';
+const FILE_PATH = process.argv.length > 2 ? process.argv[2] : '';
 app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", (_, res) => {
-  const resObject = ["This is the list of our students"];
+app.get('/students', (_, res) => {
+  const resObject = ['This is the list of our students'];
   countStudents(FILE_PATH)
     .then((msgs) => {
       for (let i = 0; i < msgs.length; i++) {
         resObject.push(msgs[i]);
       }
-      const resText = resObject.join("\n");
-      res.setHeader("Content-Type", "plain/text");
-      res.setHeader("Content-Length", resText.length);
+      const resText = resObject.join('\n');
+      res.setHeader('Content-Type', 'plain/text');
+      res.setHeader('Content-Length', resText.length);
       res.statusCode = 200;
       res.write(Buffer.from(resText));
     })
     .catch((err) => {
       resObject.push(err instanceof Error ? err.message : err.toString());
-      const resText = resObject.join("\n");
-      res.setHeader("Content-Type", "text/plain");
-      res.setHeader("Content-Length", resText.length);
+      const resText = resObject.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', resText.length);
       res.statusCode = 200;
       res.write(Buffer.from(resText));
     });
@@ -38,31 +38,31 @@ const countStudents = (path) =>
     let NUMBER_OF_STUDENTS = 0;
     let occurance = {};
 
-    fs.readFile(path, "utf-8", (err, data) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
         const toBeResolved = [];
-        data = data.split("\n");
+        data = data.split('\n');
         fields = data[0];
         for (let i = 0; i < data.length; i++) {
           if (i == 0) {
             continue;
           }
-          if (data[i] == "\r" || data[i].length <= 0) {
+          if (data[i] == '\r' || data[i].length <= 0) {
             break;
           } else {
-            first_name = data[i].split(",")[0];
-            last_name = data[i].split(",")[1];
-            age = data[i].split(",")[2];
-            field = data[i].split(",")[3].replace("\r", "");
+            first_name = data[i].split(',')[0];
+            last_name = data[i].split(',')[1];
+            age = data[i].split(',')[2];
+            field = data[i].split(',')[3].replace('\r', '');
             NUMBER_OF_STUDENTS += 1;
             file_data.push({ first_name, last_name, age, field });
           }
         }
         file_data.map((person) => {
-          field = person["field"];
+          field = person['field'];
           if (!occurance[field]) {
             occurance[field] = [person];
           } else {
@@ -77,7 +77,7 @@ const countStudents = (path) =>
           let count = 0;
           let names = [];
           for (let i = 0; i < occurance[key].length; i++) {
-            names.push(" " + occurance[key][i].first_name);
+            names.push(' ' + occurance[key][i].first_name);
             count++;
           }
           toBeResolved.push(

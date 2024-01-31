eb7d1162d45f7eb8cc2d403d42cb2ee4b0022ec3
Eugene <eugenemutuyimana15@gmail.com>
2024-01-31 22:02:41 +0200

    files tested eslint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,13 @@
-const http = require("http");
+const http = require('http');
 const PORT = 1245;
-const HOSTNAME = "127.0.0.1";
+const HOSTNAME = '127.0.0.1';
 
 const app = http.createServer((_, res) => {
-  const responeText = "Hello Holberton School!";
+  const responeText = 'Hello Holberton School!';
 
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", responeText.length);
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responeText.length);
   res.end(responeText);
 });
 
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,9 @@
-const http = require("http");
-const { stdout } = require("process");
-const fs = require("fs");
+const http = require('http');
+const { stdout } = require('process');
+const fs = require('fs');
 const PORT = 1245;
-const HOSTNAME = "127.0.0.1";
-const FILE_PATH = process.argv.length > 2 ? process.argv[2] : "";
+const HOSTNAME = '127.0.0.1';
+const FILE_PATH = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (path) =>
   new Promise((resolve, reject) => {
@@ -11,31 +11,31 @@ const countStudents = (path) =>
     let NUMBER_OF_STUDENTS = 0;
     let occurance = {};
 
-    fs.readFile(path, "utf-8", (err, data) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
         const toBeResolved = [];
-        data = data.split("\n");
+        data = data.split('\n');
         fields = data[0];
         for (let i = 0; i < data.length; i++) {
           if (i == 0) {
             continue;
           }
-          if (data[i] == "\r" || data[i].length <= 0) {
+          if (data[i] == '\r' || data[i].length <= 0) {
             break;
           } else {
-            first_name = data[i].split(",")[0];
-            last_name = data[i].split(",")[1];
-            age = data[i].split(",")[2];
-            field = data[i].split(",")[3].replace("\r", "");
+            first_name = data[i].split(',')[0];
+            last_name = data[i].split(',')[1];
+            age = data[i].split(',')[2];
+            field = data[i].split(',')[3].replace('\r', '');
             NUMBER_OF_STUDENTS += 1;
             file_data.push({ first_name, last_name, age, field });
           }
         }
         file_data.map((person) => {
-          field = person["field"];
+          field = person['field'];
           if (!occurance[field]) {
             occurance[field] = [person];
           } else {
@@ -50,7 +50,7 @@ const countStudents = (path) =>
           let count = 0;
           let names = [];
           for (let i = 0; i < occurance[key].length; i++) {
-            names.push(" " + occurance[key][i].first_name);
+            names.push(' ' + occurance[key][i].first_name);
             count++;
           }
           toBeResolved.push(
@@ -65,22 +65,22 @@ const countStudents = (path) =>
   });
 
 app = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "plain/text");
+  res.setHeader('Content-Type', 'plain/text');
   const url = req.url;
 
-  if (url == "/") {
-    res.end("Hello Holberton School!");
+  if (url == '/') {
+    res.end('Hello Holberton School!');
   }
-  if (url == "/students") {
-    const responseParts = ["This is the list of our students"];
+  if (url == '/students') {
+    const responseParts = ['This is the list of our students'];
     countStudents(FILE_PATH)
       .then((msgs) => {
         for (let i = 0; i < msgs.length; i++) {
           responseParts.push(msgs[i]);
         }
-        const resText = responseParts.join("\n");
-        res.setHeader("Content-Type", "plain/text");
-        res.setHeader("Content-Length", resText.length);
+        const resText = responseParts.join('\n');
+        res.setHeader('Content-Type', 'plain/text');
+        res.setHeader('Content-Length', resText.length);
         res.statusCode = 200;
         res.write(Buffer.from(resText));
       })

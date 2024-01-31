d806fd4e0ef107d56d4919b9a936e8899038d019
Eugene <eugenemutuyimana15@gmail.com>
2024-01-31 22:22:22 +0200

    used stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
-  INPUT = process.stdin.read();
-  if (INPUT) console.log(`Your name is: ${INPUT}`);
+  const INPUT = process.stdin.read();
+  if (INPUT) process.stdout.write(`Your name is: ${INPUT}`);
 });
+
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });

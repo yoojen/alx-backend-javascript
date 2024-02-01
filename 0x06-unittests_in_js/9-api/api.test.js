e8bd6a2029eef695a0feed38342a3cc36f0cd748
Eugene <eugenemutuyimana15@gmail.com>
2024-02-02 00:25:48 +0200

    added test for / route

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 describe('API test suite', () => {
   it('GET / returns correct response', (done) => {
-    request.get(`http://localhost:7865/`, (_err, res, body) => {
+    request.get('http://localhost:7865/', (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();

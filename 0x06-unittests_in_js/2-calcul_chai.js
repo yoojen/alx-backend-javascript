be93f38404e5cfde750a2a06f2755dad9bdc5991
Eugene <eugenemutuyimana15@gmail.com>
2024-02-01 13:12:01 +0200

    changed switch case to if statemnets

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,18 +1,18 @@
 function calculateNumber(type, a, b) {
-  switch (type) {
-    case 'SUM':
-      return Math.round(a) + Math.round(b);
-    case 'SUBTRACT':
-      return Math.round(a) - Math.round(b);
-    case 'DIVIDE':
-      if (Math.round(b) == 0) {
-        return 'Error';
-      } else {
-        return Math.round(a) / Math.round(b);
-      }
-    default:
-      return 0;
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
   }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    } else {
+      return Math.round(a) / Math.round(b);
+    }
+  }
+  return 0;
 }
 
 module.exports = calculateNumber;

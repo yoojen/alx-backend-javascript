c2fa1c4f091d448d59210e054cc6a0e3d56f10a3
Eugene <eugenemutuyimana15@gmail.com>
2024-02-01 13:13:39 +0200

    changed switch case to if statemnets

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,4 +1,4 @@
-function calculateNumber(type, a, b) {
+const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   }
@@ -6,13 +6,9 @@ function calculateNumber(type, a, b) {
     return Math.round(a) - Math.round(b);
   }
   if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
-      return 'Error';
-    } else {
-      return Math.round(a) / Math.round(b);
-    }
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
   return 0;
-}
+};
 
 module.exports = calculateNumber;
